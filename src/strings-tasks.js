a9095561774c52ee5e2eb9ed4664bd0808bfcf6c
Vlad93 <selukvlad@mail.ru>
2023-11-24 11:54:28 +0300

    feat: formatTime task done

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -214,8 +214,8 @@ function sumOfCodes(str) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
@@ -229,8 +229,8 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
@@ -246,8 +246,19 @@ function endsWith(/* str, substr */) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  function isDoubleChar(str) {
+    return str.length === 2;
+  }
+  let formatMinutes = String(minutes);
+  let formatSeconds = String(seconds);
+  if (!isDoubleChar(formatMinutes)) {
+    formatMinutes = `0${formatMinutes}`;
+  }
+  if (!isDoubleChar(formatSeconds)) {
+    formatSeconds = `0${formatSeconds}`;
+  }
+  return `${formatMinutes}:${formatSeconds}`;
 }
 
 /**

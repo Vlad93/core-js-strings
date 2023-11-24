8bb6192c5f6dd8578e7822293609b3dd794707cb
Vlad93 <selukvlad@mail.ru>
2023-11-24 13:19:26 +0300

    feat: findLongestWord task done

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -358,8 +358,11 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  const wordsArr = sentence.split(' ');
+  return wordsArr.sort((a, b) => {
+    return b.length - a.length;
+  })[0];
 }
 
 /**

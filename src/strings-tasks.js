ecf088a622bbb1ffd54342febd2bef827b61e991
Vlad93 <selukvlad@mail.ru>
2023-11-24 12:31:13 +0300

    countVowels task done

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -315,8 +315,15 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowelsStr = 'aeiouy';
+  let count = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    if (vowelsStr.includes(str[i].toLowerCase())) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 /**
